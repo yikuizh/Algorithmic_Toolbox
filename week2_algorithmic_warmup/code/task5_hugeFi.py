0b4a3642223ca78b21548a3b56e59e5e64ae933b
yikuizh <zhangyikui0321@163.com>
2023-03-17 02:27:47 +0100

    update task 5 (pisaro period) and test.py

--- week2_algorithmic_warmup/code/task5_hugeFi.py
+++ week2_algorithmic_warmup/code/task5_hugeFi.py
@@ -1,24 +1,50 @@
 import numpy as np
 
-def hugeFi(n,m): # version to avoid RuntimeWarning: overflow encountered in long_scalars
-# too long run time -- solved by pisano period
-    if n<=1:
-        return n
+def modarray(n,m): # version to avoid RuntimeWarning: overflow encountered in long_scalars
+    
+    # too long run time -- solved by pisano period
+    
+    #  basic idea here: for each m, find the sequence of each Fi(mod(m)) -- until the sequence loop again (until 0, 1 shows up again)
+
     # no need a else here
+    mod_array = [0, 1]
+
     pre_mod = 0
 
     cur_mod = 1
 
-    for _ in range(n-1): # directly use number to represent rather than the array
-        temp = pre_mod
-        
-        pre_mod = Fn + Fn_1
-        
-        Fn = temp
+    pre_fi = 0
 
-    return  Fn_1 % m
+    cur_fi = 1
 
+    while not (pre_mod == 0 and cur_mod == 1 and pre_fi!=0): # while not -- can use three conditions
+
+     # directly use number to represent rather than the array -- find the mod sequence
+      
+        temp = cur_fi
+
+        cur_fi = pre_fi + cur_fi 
+
+        pre_fi = temp
+
+        pre_mod = cur_mod
+
+        cur_mod = cur_fi % m
+
+        mod_array.append(cur_mod)
+
+    return mod_array
+
+def hugeFi(n,m):
+
+    if n<=1:
+        return n
+
+    mod_array = modarray(n,m)
+    
+    return mod_array[n%(len(mod_array)-2)] # exclude pre_mod -- 0 and cur_mod -- 1 (since the array include pre_mod and cur_mod outside the boundary)
 
 if __name__ == '__main__':
     n,m = map(int, input().split())
     print(hugeFi(n, m))
+# print(hugeFi(100, 100))
